93b15ec89d87d1ef6a633fd2dc4916bfbe4a2f30
Hodocharles <110722726+Hodocharles@users.noreply.github.com>
2023-07-01 00:01:34 +0100

    This is my first project on bank app
    Testing my mobile bank app

---
+++ Transfer.js
@@ -0,0 +1,16 @@
+function showDiv(select){
+   if(select.value==1){
+    document.getElementById('hidden_div').style.display = "block";
+   } else{
+    document.getElementById('hidden_div').style.display = "none";
+   }
+} 
+
+
+function showEle(select){
+   if(select.value==1){
+    document.getElementById('hidden').style.display = "block";
+   } else{
+    document.getElementById('hidden').style.display = "none";
+   }
+} 
